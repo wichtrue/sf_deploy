a6446defe6c81afb79ec19473341ea98dd88844f
Warawich Sureepitak <warawich@gmail.com>
2018-12-24 18:48:27 +0700

    Initial

---
+++ sf_deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+DATE = `date +%Y%m%d`
+
+eval `ssh-agent`
+
+ssh-add /root/.ssh/id_rsa
+
+ssh root@192.168.1.26 tar czf /tmp/nlu.tgz /opt/nlu_platform/
+
+scp -pr root@192.168.1.26:/tmp/nlu.tgz /tmp/.
+
+ssh root@192.168.1.26 rm -rvf /tmp/nlu.tgz
+
+scp -pr  /tmp/nlu.tgz root@172.30.149.188:/opt/.
+
+rm -rvf /tmp/nlu.tgz
+
+ssh root@172.30.149.188 mv -v /opt/nlu_platform/ /opt/nlu_platform_$DATE
+
+ssh root@172.30.149.188 tar xzf /opt/nlu.tgz 
+
+ssh root@172.30.149.188 rm -rvf /opt/nlu.tgz
+
+ssh root@172.30.149.188 systemctl restart nlu
+
+ssh root@172.30.149.188 systemctl restart nginx 
+
+eval `ssh-agent -k`
